bc03d64026977d860ce9d51dd94b94c223f30f07
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2021-11-02 19:43:27 -0300

    add note and get notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,45 @@
-// const getNotes = 'teste'
+const fs = require('fs')
+const dataFileName = 'notes.json'
 
 const getNotes = function(){
-    return 'Your notes'
+    try{
+        const notes = fs.readFileSync(dataFileName)
+        return JSON.parse(notes)
+    }catch(e){
+        return [];
+    }
 }
 
-module.exports = getNotes
+const addNote = function (title, body){
+    const notes = getNotes()
+    const duplicated = notes.filter(function (note){
+        return note.title === title
+    })
+
+    if(duplicated.length == 0){
+        notes.push({
+            'title': title,
+            'body': body
+        })
+    
+        saveNotes(notes)
+        console.log('Note added!')
+    }else{
+        const stringNote = JSON.stringify({
+            'title': title,
+            'body': body
+        })
+        console.log('Note ', stringNote, ' already exits!')
+    }
+}
+
+const saveNotes = function (notes){
+    const jsonString = JSON.stringify(notes)
+    fs.writeFileSync(dataFileName, jsonString)
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    saveNotes: saveNotes
+}
