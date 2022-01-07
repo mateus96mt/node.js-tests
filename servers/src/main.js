162a4ed7827cefd8eb443332aea7ecac85890995
Mateus Teixeira Magalhães <mateus.magalhaes@levty.com>
2022-01-07 09:37:00 -0300

    using html files in express

--- servers/src/main.js
+++ servers/src/main.js
@@ -1,6 +1,17 @@
+const path = require('path')
 const express = require('express');
 
 const app = express();
+
+console.log(__dirname)
+console.log(__filename)
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath)
+
+//config express path
+app.use(express.static(publicPath))
+
+let html = `hey hou`
 
 // app.com
 // app.com/help
@@ -19,9 +30,10 @@ app.get('/about', (req, res)=>{
 })
 
 app.get('/weather', (req, res)=>{
-    res.send('weather page!')
+    res.send(html)
 })
 
 app.listen(3000, ()=>{
     console.log('listening on')
 })
+
